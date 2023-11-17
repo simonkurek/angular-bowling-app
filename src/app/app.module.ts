c29b9daccf1e75aaa24c41ce2dfdaf947c047b00
Simon Kurek <simonkurek@protonmail.com>
2023-11-17 14:06:05 +0100

    feat(Header): extracted to dedicated 'dump' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ViewsModule } from './views/views.module';
 import { BowlingService } from './services/bowling.service';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ViewsModule],
+  imports: [BrowserModule, ViewsModule, UiModule],
   providers: [BowlingService],
   bootstrap: [AppComponent],
 })
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { DataShowComponent } from './data-show/data-show.component';
 import { DataInputComponent } from './data-input/data-input.component';
 import { BowlingService } from '../services/bowling.service';
+import { UiModule } from '../ui/ui.module';
 
 @NgModule({
   declarations: [DataShowComponent, DataInputComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, UiModule],
   exports: [DataShowComponent, DataInputComponent],
 })
 export class ViewsModule {}
